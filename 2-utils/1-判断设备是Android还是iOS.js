73648c8a83f2cf428408d6179fb0df53078305b7
xiaomajia <1134126206@qq.com>
2018-11-26 14:02:49 +0800

    update utils/

---
+++ 2-utils/1-判断设备是Android还是iOS.js
@@ -0,0 +1,34 @@
+// 方法一
+var u = navigator.userAgent;
+var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+console.log('是否是Android：' + isAndroid);
+console.log('是否是iOS：' + isiOS);
+
+// 方法二：判断iPhone | iPad | iPod | iOS | Android客户端
+if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) { //判断iPhone|iPad|iPod|iOS
+    // console.log(navigator.userAgent);  
+    window.location.href = "iPhone.html";
+} else if (/(Android)/i.test(navigator.userAgent)) { //判断Android
+    // console.log(navigator.userAgent); 
+    window.location.href = "Android.html";
+} else {
+    //pc
+    window.location.href = "pc.html";
+};
+
+// 方法三：判断pc还是移动端
+// 1. 判断是否手机端访问
+var userAgentInfo = navigator.userAgent.toLowerCase();
+var Agents = ["android", "iphone",
+    "symbianos", "windows phone",
+    "ipad", "ipod"
+];
+// 2. 返回导航到当前网页的超链接所在网页的URL
+var ly = document.referrer;
+// 3. 执行判断，导向wap网址
+for (var v = 0; v < Agents.length; v++) {
+    if (userAgentInfo.indexOf(Agents[v]) >= 0 && (ly == "" || ly == null)) {
+        this.location.href = 'http://m.***.com'; //wap端地址
+    }
+}
---
+++ 2-utils/2-判断浏览器终端类型.js
@@ -0,0 +1,36 @@
+// 定义终端判断对象
+var browser = {
+    versions: function () {
+        var u = navigator.userAgent,
+            app = navigator.appVersion;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
+            iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, //是否iPad
+            webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+            qq: u.match(/\sQQ/i) == " qq" //是否QQ
+        };
+    }(),
+    language: (navigator.browserLanguage || navigator.language).toLowerCase()
+}
+// 执行判断 --> 根据browser.versions.xxx 的值为true / false
+
+//判断是否webKit内核
+if (browser.versions.webKit) {
+    console.log("is webKit");
+}
+// 判断浏览器语言
+currentLang = navigator.language; //判断除IE外其他浏览器使用语言
+if (!currentLang) { //判断IE浏览器使用语言
+    currentLang = navigator.browserLanguage;
+}
+console.log(currentLang); // zh-CN 等
+
+console.log(document.referrer);
