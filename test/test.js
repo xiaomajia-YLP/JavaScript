9e164ba4c01b3b83a4d5a3ae818601a64e60ad9e
xiaomajia <1134126206@qq.com>
2018-12-19 22:30:19 +0800

    Graph

--- test/test.js
+++ test/test.js
@@ -56,7 +56,7 @@ class Graph {
 			this.vertices[vertex.id] = vertex
 		}
 		// 给edge添加id索引
-		const edgesWithId = edges.map(function(edge, i) {
+		const edgesWithId = edges.map(function (edge, i) {
 			edge.id = i + 1
 			return edge
 		})
@@ -79,12 +79,84 @@ class Graph {
 			this.edgeRelations[edge.rightId].push(edge.id)
 		}
 	}
-	getVertex(vertexId){
+	// 获取某一个顶点
+	getVertex(vertexId) {
 		if (!_.includes(this.vertices, vertexId)) {
 			return null
 		} else {
 			return this.vertices[vertexId]
 		}
+	}
+	// 遍历顶点/边
+	// （数组都是自带有序特性的，但是在关系图谱的定义中，顶点之间并不存在顺序。所以我们自然也不会允许对图对象中的顶点进行直接的循环操作，而采用回调函数的方式进行循环，以模糊其有序性。）
+	eachVertices(callbackFunc) {
+		const self = this
+		return self.vertexIds.forEach(function (vertexId) {
+			return callbackFunc(self.vertices[vertexId])
+		})
+	}
+	eachEdges(callbackFunc) {
+		const self = this
+		return self.edgeIds.forEach(function (edgeId) {
+			return callbackFunc(self.edges[edgeId])
+		})
+	}
+	// 特征值
+	// 1. degree
+	// (1) 得到一个顶点的相邻边数组
+	getEdgesByVertexId(vertexId) {
+		if (!_.includes(this.vertexIds, vertexId)) {
+			return []
+		}
+
+		if (!_.has(this.edgeRelations, vertexId)) {
+			return []
+		}
+
+		const self = this
+
+		return self.edgeRelations[vertexId].map(function (edgeId) {
+			return self.edges[edgeId]
+		})
+	}
+	// (2) 获得一个顶点的度
+	degree(vertexId) {
+		return this.getEdgesByVertexId(vertexId).length
+	}
+	// 2. max degree
+	// (1) 用于找出带有最大度数的顶点
+	largestVertex() {
+		const self = this
+
+		const degrees = self.vertexIds.map(function (vertexId) {
+			return {
+				degree: self.degree(vertexId),
+				id: vertexId
+			}
+
+		})
+
+		return self.getVertex(_.maxBy(degrees, 'degree').id)
+	}
+	// (2) 用于获取其度数
+	maxDegree() {
+		return this.degree(this.largestVertex().id)
+	}
+	// 3. Average Degree
+	// 平均度 = 该图中边的数量 * 2 / 顶点的数量
+	avgDegree() {
+		return 2 * this.edgeIds.length / this.vertexIds.length
+	}
+	// 4. Self-Loop 自环：在一个关系图谱内，某一个顶点存在一条边再与自己相连（可以被解释为某一个事件的连续发生可能性）
+	loops() {
+		const self = this
+		return self.edgeIds
+			.map(function(edgeId) {
+				return self.edges[edgeId]
+			})
+			.filter(function(edge) {
+				return edge.leftId === edge.rightId
+			})
 	}
 }
 
